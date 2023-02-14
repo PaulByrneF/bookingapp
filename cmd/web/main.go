d56731e4faa161ce277f2e80ee559c34fefdc326
Paul Byrne <paul.byrne.f@gmail.com>
2023-02-14 21:12:36 +0000

    adding web application skeleton

---
+++ cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/paulbyrne/bookingapp/pkg/config"
+	"github.com/paulbyrne/bookingapp/pkg/handlers"
+	"github.com/paulbyrne/bookingapp/pkg/render"
+)
+
+const port = ":8080"
+
+var appConfig config.AppConfig
+var session *scs.SessionManager
+
+// main the the main function which is the entry point to the web application
+func main() {
+
+	appConfig.InProduction = false
+
+	// session config
+	session = scs.New()
+	session.Lifetime = 45 * time.Minute
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = appConfig.InProduction
+
+	// set session in appConfig
+	appConfig.Session = session
+
+	// create web template
+	tempCache, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	// update template cache in appConfig
+	appConfig.TemplateCache = tempCache
+	appConfig.UseCache = false
+
+	repo := handlers.NewRepo(&appConfig)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&appConfig)
+
+	fmt.Println(fmt.Sprintf("Starting server on %s | http://localhost:8080", port))
+
+	serve := &http.Server{
+		Addr:    port,
+		Handler: routes(&appConfig),
+	}
+
+	err = serve.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/paulbyrne/bookingapp/pkg/config"
+	"github.com/paulbyrne/bookingapp/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	mux := chi.NewRouter()
+
+	//prints stack trace when panic & recover application
+	mux.Use(middleware.Recoverer)
+
+	// intercept and print request url
+	mux.Use(WriteToConsole)
+
+	// csrf protection
+	mux.Use(NoSurf)
+
+	// access session
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
