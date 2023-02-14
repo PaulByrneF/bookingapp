5ee26a6aa2a8e18115a39cb377108a0aa358fa9e
Paul Byrne <paul.byrne.f@gmail.com>
2023-02-14 22:09:49 +0000

    added fileserver to serve static files from the static directory at root

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,5 +27,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// fileserver for serving static files
+	fileserver := http.FileServer(http.Dir("../../static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
+
 	return mux
 }
